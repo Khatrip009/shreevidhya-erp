e9ae72a41e389594ed8e30154417a98eb68b4921
Khatrip009 <Khatrip.009@gmail.com>
2026-06-06 18:14:01 +0530

    Updates

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'shreevidhya-v3';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(cacheNames.map((name) => caches.delete(name)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+});
